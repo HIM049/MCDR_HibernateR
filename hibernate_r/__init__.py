3335f6c1d3ee846d99d14cfdd4b131d63ac15b1b
HIM049 <HIM_049@163.com>
2025-02-20 14:29:35 +0800

    fix: Compatibility with backup plugin

--- hibernate_r/__init__.py
+++ hibernate_r/__init__.py
@@ -16,6 +16,8 @@ timer_manager = TimerManager()
 # 创建 fake_server_socket 实例
 fake_server_socket = None
 
+wish_server_status = True
+
 
 # 初始化插件
 def on_load(server: PluginServerInterface, prev_module):
@@ -25,7 +27,7 @@ def on_load(server: PluginServerInterface, prev_module):
     builder = SimpleCommandBuilder()
     builder.command('!!hr sleep', lambda src: hr_sleep(src.get_server()))
     builder.command('!!hr wakeup', lambda src: hr_wakeup(src.get_server()))
-    builder.command('!!hr wakeup fs', lambda src: fake_server_socket.start(src.get_server()))
+    builder.command('!!hr wakeup fs', lambda src: fake_server_socket.start(src.get_server(), start_server))
     builder.register(server)
 
     # 检查配置文件
@@ -38,8 +40,9 @@ def on_load(server: PluginServerInterface, prev_module):
 
     # 检查服务器状态并启动计时器或伪装服务器
     if server.is_server_running() or server.is_server_startup():
+        wish_server_status = True
         server.logger.info("服务器正在运行，启动计时器")
-        timer_manager.start_timer(server)
+        timer_manager.start_timer(server, stop_server)
     else:
 
         server.logger.warning("无法判断当前服务器状态，请使用 !!hr start fs 手动启动伪装服务器")
@@ -52,13 +55,12 @@ def on_unload(server: PluginServerInterface):
     fake_server_socket.stop(server)
     server.logger.info("插件已卸载")
 
-
 # 手动休眠
 @new_thread
 def hr_sleep(server: PluginServerInterface):
     server.logger.info("事件：手动休眠")
     timer_manager.cancel_timer(server)
-    server.stop()
+    stop_server()
 
 # 手动唤醒
 @new_thread
@@ -66,7 +68,7 @@ def hr_wakeup(server: PluginServerInterface):
 
     server.logger.info("事件：手动唤醒")
     if fake_server_socket.stop(server):
-        server.start()
+        start_server()
     else:
         server.logger.info("伪装服务器关闭失败，无法手动唤醒")
 
@@ -75,9 +77,10 @@ def hr_wakeup(server: PluginServerInterface):
 # 服务器启动完成事件
 @new_thread
 def on_server_startup(server: PluginServerInterface):
+    wish_server_status = True
     server.logger.info("事件：服务器启动")
     time.sleep(5)
-    timer_manager.start_timer(server)
+    timer_manager.start_timer(server, stop_server)
 
 # 玩家加入事件
 @new_thread
@@ -93,7 +96,7 @@ def on_player_left(server: PluginServerInterface, player):
     server.logger.info("事件：玩家退出")
     time.sleep(2)
     if server.is_server_running():
-        timer_manager.start_timer(server)
+        timer_manager.start_timer(server, stop_server)
 
 
 
@@ -101,6 +104,20 @@ def on_player_left(server: PluginServerInterface, player):
 def on_server_stop(server: PluginServerInterface,  server_return_code: int):
     server.logger.info("事件：服务器关闭")
     timer_manager.cancel_timer(server)
-    fake_server_socket.start(server)
+    # 匹配预期状态
+    if wish_server_status != False:
+        server.logger.warning("意外的服务器关闭，不启动伪装服务器")
+    else:
+        fake_server_socket.start(server, start_server)
 
 
+
+# 主动关闭服务器
+def stop_server(server: PluginServerInterface):
+    wish_server_status = False
+    server.stop()
+
+# 主动开启服务器
+def start_server(server: PluginServerInterface):
+    wish_server_status = True
+    server.start()
